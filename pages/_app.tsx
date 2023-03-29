b9a07a1469777ffa93f2e4c021572b6102840495
moazzam2019 <moazzam.iqbal2019@gmail.com>
2023-03-29 19:27:48 +0500

    development branch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,24 @@
-import App from 'next/app';
-import React from 'react';
-import { Provider } from 'react-redux';
-import nextCookie from 'next-cookies';
-import withReduxSaga from '@redux/withReduxSaga';
-import { Store } from 'redux';
-import BaseLayout from '@layouts/base-layout';
-import {
-  authService, userService, settingService
-} from '@services/index';
-import Router from 'next/router';
-import { NextPageContext } from 'next';
-import { loginSuccess } from '@redux/auth/actions';
-import { updateCurrentUser } from '@redux/user/actions';
-import { updateUIValue } from '@redux/ui/actions';
-import { Socket } from 'src/socket';
-import Head from 'next/head';
-import { SETTING_KEYS } from 'src/constants';
-import { pick } from 'lodash';
-import { updateLiveStreamSettings } from '@redux/streaming/actions';
-import { updateSettings } from '@redux/settings/actions';
-import '../style/index.less';
-import { setGlobalConfig } from '@services/config';
+import App from "next/app";
+import React from "react";
+import { Provider } from "react-redux";
+import nextCookie from "next-cookies";
+import withReduxSaga from "@redux/withReduxSaga";
+import { Store } from "redux";
+import BaseLayout from "@layouts/base-layout";
+import { authService, userService, settingService } from "@services/index";
+import Router from "next/router";
+import { NextPageContext } from "next";
+import { loginSuccess } from "@redux/auth/actions";
+import { updateCurrentUser } from "@redux/user/actions";
+import { updateUIValue } from "@redux/ui/actions";
+import { Socket } from "src/socket";
+import Head from "next/head";
+import { SETTING_KEYS } from "src/constants";
+import { pick } from "lodash";
+import { updateLiveStreamSettings } from "@redux/streaming/actions";
+import { updateSettings } from "@redux/settings/actions";
+import "../style/index.less";
+import { setGlobalConfig } from "@services/config";
 
 declare global {
   interface Window {
@@ -29,18 +27,18 @@ declare global {
     iframely: any;
   }
 }
-
+// Function
 function redirectLogin(ctx: any) {
   if (process.browser) {
     authService.removeToken();
-    Router.push('/');
+    Router.push("/");
     return;
   }
 
   // fix for production build
-  ctx.res.clearCookie && ctx.res.clearCookie('token');
-  ctx.res.clearCookie && ctx.res.clearCookie('role');
-  ctx.res.writeHead && ctx.res.writeHead(302, { Location: '/' });
+  ctx.res.clearCookie && ctx.res.clearCookie("token");
+  ctx.res.clearCookie && ctx.res.clearCookie("role");
+  ctx.res.writeHead && ctx.res.writeHead(302, { Location: "/" });
   ctx.res.end && ctx.res.end();
 }
 
@@ -59,7 +57,7 @@ async function auth(
     if (token) {
       authService.setToken(token);
       const user = await userService.me({
-        Authorization: token
+        Authorization: token,
       });
       if (!user.data || !user.data._id) {
         redirectLogin(ctx);
@@ -84,15 +82,15 @@ async function updateSettingsStore(ctx: NextPageContext, settings) {
   const { store } = ctx;
   store.dispatch(
     updateUIValue({
-      logo: settings.logoUrl || '',
-      siteName: settings.siteName || '',
-      favicon: settings.favicon || '',
-      loginPlaceholderImage: settings.loginPlaceholderImage || '',
+      logo: settings.logoUrl || "",
+      siteName: settings.siteName || "",
+      favicon: settings.favicon || "",
+      loginPlaceholderImage: settings.loginPlaceholderImage || "",
       menus: settings.menus || [],
-      footerContent: settings.footerContent || '',
+      footerContent: settings.footerContent || "",
       countries: settings.countries || [],
-      userBenefit: settings.userBenefit || '',
-      modelBenefit: settings.modelBenefit || ''
+      userBenefit: settings.userBenefit || "",
+      modelBenefit: settings.modelBenefit || "",
     })
   );
   store.dispatch(
@@ -106,7 +104,7 @@ async function updateSettingsStore(ctx: NextPageContext, settings) {
         SETTING_KEYS.SECURE_OPTION,
         SETTING_KEYS.ANT_MEDIA_APPNAME,
         SETTING_KEYS.AGORA_APPID,
-        SETTING_KEYS.AGORA_ENABLE
+        SETTING_KEYS.AGORA_ENABLE,
       ])
     )
   );
@@ -123,7 +121,7 @@ async function updateSettingsStore(ctx: NextPageContext, settings) {
         SETTING_KEYS.TWITTER_CLIENT_ID,
         SETTING_KEYS.PAYMENT_GATEWAY,
         SETTING_KEYS.META_KEYWORDS,
-        SETTING_KEYS.META_DESCRIPTION
+        SETTING_KEYS.META_DESCRIPTION,
       ])
     )
   );
@@ -149,7 +147,7 @@ class Application extends App<IApp> {
     // load configuration from ENV and put to config
     if (!process.browser) {
       // eslint-disable-next-line global-require
-      const dotenv = require('dotenv');
+      const dotenv = require("dotenv");
       const myEnv = dotenv.config().parsed;
 
       // publish to server config with app
@@ -157,7 +155,7 @@ class Application extends App<IApp> {
 
       // load public config and api-endpoint?
       Object.keys(myEnv).forEach((key) => {
-        if (key.indexOf('NEXT_PUBLIC_') === 0) {
+        if (key.indexOf("NEXT_PUBLIC_") === 0) {
           publicConfig[key] = myEnv[key];
         }
       });
@@ -170,13 +168,11 @@ class Application extends App<IApp> {
       await auth(ctx, noredirect, onlyPerformer);
     }
     const { token } = nextCookie(ctx);
-    ctx.token = token || '';
+    ctx.token = token || "";
     // server side to load settings, once time only
     let settings = {};
     if (!process.browser) {
-      const [setting] = await Promise.all([
-        settingService.all('all', true)
-      ]);
+      const [setting] = await Promise.all([settingService.all("all", true)]);
       settings = { ...setting.data };
       await updateSettingsStore(ctx, settings);
     }
@@ -188,7 +184,7 @@ class Application extends App<IApp> {
       settings,
       pageProps,
       layout: Component.layout,
-      config: publicConfig
+      config: publicConfig,
     };
   }
 
@@ -198,30 +194,35 @@ class Application extends App<IApp> {
   }
 
   render() {
-    const {
-      Component, pageProps, store, settings
-    } = this.props;
+    const { Component, pageProps, store, settings } = this.props;
     const { layout } = Component;
     return (
       <Provider store={store}>
         <Head>
           <title>{settings?.siteName}</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
           {/* GA code */}
-          {settings && settings.gaCode && [
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${settings.gaCode}`} />,
-            <script
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{
-                __html: `
+          {settings &&
+            settings.gaCode && [
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${settings.gaCode}`}
+              />,
+              <script
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: `
                  window.dataLayer = window.dataLayer || [];
                  function gtag(){dataLayer.push(arguments);}
                  gtag('js', new Date());
                  gtag('config', '${settings.gaCode}');
-             `
-              }}
-            />
-          ]}
+             `,
+                }}
+              />,
+            ]}
           {/* extra script */}
           {settings && settings.headerScript && (
             // eslint-disable-next-line react/no-danger
